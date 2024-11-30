26db469c0fe828647ace853c5ff3922e1147131e
Tran Ngo Hung Vi <vitnhhe173419@fpt.edu>
2024-11-30 10:42:11 +0700

    update : addpost Screen

--- src/App.js
+++ src/App.js
@@ -3,6 +3,8 @@ import { BrowserRouter, Route, Routes } from "react-router-dom";
 import LoginScreen from "./login_register/LoginScreen";
 import ForgotPassword from "./login_register/ForgotPassword";
 import Register from "./login_register/Register";
+import AddPost from "./login_register/AddPost";
+
 
 function App() {
   return (
@@ -11,6 +13,7 @@ function App() {
         <Route path="/" element={<LoginScreen />} />
         <Route path="/forgot-password" element={<ForgotPassword />} />
         <Route path="/sign-up" element={<Register />} />
+        <Route path="/add-post" element={<AddPost />} />
       </Routes>
     </BrowserRouter>
   );

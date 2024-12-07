440a678022b7c48735a45fec79d73102d53997d5
Tran Ngo Hung Vi <vitnhhe173419@fpt.edu>
2024-12-07 10:13:18 +0700

    fix: login Screen

--- src/login_register/LoginScreen.js
+++ src/login_register/LoginScreen.js
@@ -1,52 +1,9 @@
-  import React, { useState } from "react";
-  import { useNavigate } from "react-router-dom";
-  import "./util.css";
-  import "./main.css";
-  import { Link } from "react-router-dom";
+import React, { useState } from "react";
+import { useNavigate } from "react-router-dom";
+import "./util.css";
+import "./main.css";
+import { Link } from "react-router-dom";
 
-  const LoginScreen = ({ onLogin }) => {
-    // Nhận props onLogin từ App.js
-    const [username, setUsername] = useState("");
-    const [password, setPassword] = useState("");
-    const [error, setError] = useState("");
-    const navigate = useNavigate();
-
-    const handleLogin = async (e) => {
-      e.preventDefault();
-      try {
-        // Fetch dữ liệu từ file database.json
-        const response = await fetch("/database.json");
-        if (!response.ok) {
-          throw new Error("Failed to load data");
-        }
-
-        const data = await response.json(); // Parse dữ liệu JSON
-
-        // Kiểm tra thông tin người dùng
-        const user = data.users.find((user) => user.username === username);
-        if (user && user.passwordHash === password) {
-          onLogin({
-            id:user.id,
-            username: user.username,
-          
-          }); // Gửi tên người dùng lên App.js khi đăng nhập thành công
-          navigate("/home");
-        } else {
-          setError(
-            <p
-              style={{
-                fontFamily: "Ubuntu, sans-serif",
-                marginBottom: "10px",
-                textAlign: "center",
-                color: "red",
-                fontSize: "16px",
-              }}
-            >
-              Incorrect account or password!
-            </p>
-          );
-        }
-      } catch (error) {
 const LoginScreen = ({ onLogin }) => {
   const [username, setUsername] = useState("");
   const [password, setPassword] = useState("");
@@ -56,12 +13,13 @@ const LoginScreen = ({ onLogin }) => {
   const handleLogin = async (e) => {
     e.preventDefault();
     try {
+      // Fetch dữ liệu từ file database.json
       const response = await fetch("/database.json");
       if (!response.ok) {
         throw new Error("Failed to load data");
       }
 
-      const data = await response.json();
+      const data = await response.json(); // Parse dữ liệu JSON
 
       // Kiểm tra thông tin người dùng
       const user = data.users.find((user) => user.username === username);
@@ -82,115 +40,132 @@ const LoginScreen = ({ onLogin }) => {
               fontSize: "16px",
             }}
           >
-            Something went wrong!
+            Incorrect account or password!
           </p>
         );
       }
-    };
-
-    return (
-      <div className="limiter">
-        <div className="container-login100">
-          <div className="wrap-login100 p-t-30 p-b-50">
-            <span
-              className="login100-form-title p-b-41"
-              style={{ fontWeight: "bold", fontFamily: "Ubuntu, sans-serif" }}
-            >
-              Account Login
-            </span>
-            <form
-              className="login100-form validate-form p-b-33 p-t-5"
-              onSubmit={handleLogin}
-            >
-              <div
-                className="wrap-input100 validate-input"
-                data-validate="Enter username"
-              >
-                <input
-                  className="input100"
-                  type="text"
-                  name="username"
-                  placeholder="User name"
-                  value={username}
-                  onChange={(e) => setUsername(e.target.value)}
-                  style={{ fontFamily: "Ubuntu, sans-serif" }}
-                />
-                <span
-                  className="focus-input100"
-                  data-placeholder="&#xe82a;"
-                ></span>
-              </div>
-
-              <div
-                className="wrap-input100 validate-input"
-                data-validate="Enter password"
-              >
-                <input
-                  className="input100"
-                  type="password"
-                  name="pass"
-                  placeholder="Password"
-                  value={password}
-                  onChange={(e) => setPassword(e.target.value)}
-                  style={{ fontFamily: "Ubuntu, sans-serif" }}
-                />
-                <span
-                  className="focus-input100"
-                  data-placeholder="&#xe80f;"
-                ></span>
-              </div>
-
-              {error && (
-                <div
-                  style={{
-                    color: "red",
-                    fontFamily: "Ubuntu, sans-serif",
-                    marginBottom: "10px",
-                  }}
-                >
-                  {error}
-                </div>
-              )}
-
-              <div className="container-login100-form-btn m-t-32">
-                <button
-                  type="submit"
-                  className="login100-form-btn"
-                  style={{ fontFamily: "Ubuntu, sans-serif" }}
-                >
-                  Login
-                </button>
-              </div>
-
-              <div className="text-center p-t-20">
-                <Link
-                  to="/forgot-password"
-                  className="txt1"
-                  style={{
-                    fontFamily: "Ubuntu, sans-serif",
-                    display: "block",
-                    marginBottom: "10px",
-                    color: "#d41872",
-                  }}
-                >
-                  Forgot Password?
-                </Link>
-                <p>
-                  Don’t have an account?
-                  <Link
-                    to="/sign-up"
-                    className="txt1"
-                    style={{ fontFamily: "Ubuntu, sans-serif", color: "#d41872" }}
-                  >
-                    Sign up
-                  </Link>
-                </p>
-              </div>
-            </form>
-          </div>
-        </div>
-      </div>
-    );
+    } catch (error) {
+      // Bắt lỗi nếu có lỗi trong try
+      console.error("Error during login:", error);
+      setError(
+        <p
+          style={{
+            fontFamily: "Ubuntu, sans-serif",
+            marginBottom: "10px",
+            textAlign: "center",
+            color: "red",
+            fontSize: "16px",
+          }}
+        >
+          An error occurred while logging in. Please try again.
+        </p>
+      );
+    }
   };
 
-  export default LoginScreen;
+  return (
+    <div className="limiter">
+      <div className="container-login100">
+        <div className="wrap-login100 p-t-30 p-b-50">
+          <span
+            className="login100-form-title p-b-41"
+            style={{ fontWeight: "bold", fontFamily: "Ubuntu, sans-serif" }}
+          >
+            Account Login
+          </span>
+          <form
+            className="login100-form validate-form p-b-33 p-t-5"
+            onSubmit={handleLogin}
+          >
+            <div
+              className="wrap-input100 validate-input"
+              data-validate="Enter username"
+            >
+              <input
+                className="input100"
+                type="text"
+                name="username"
+                placeholder="User name"
+                value={username}
+                onChange={(e) => setUsername(e.target.value)}
+                style={{ fontFamily: "Ubuntu, sans-serif" }}
+              />
+              <span
+                className="focus-input100"
+                data-placeholder="&#xe82a;"
+              ></span>
+            </div>
+
+            <div
+              className="wrap-input100 validate-input"
+              data-validate="Enter password"
+            >
+              <input
+                className="input100"
+                type="password"
+                name="pass"
+                placeholder="Password"
+                value={password}
+                onChange={(e) => setPassword(e.target.value)}
+                style={{ fontFamily: "Ubuntu, sans-serif" }}
+              />
+              <span
+                className="focus-input100"
+                data-placeholder="&#xe80f;"
+              ></span>
+            </div>
+
+            {error && (
+              <div
+                style={{
+                  color: "red",
+                  fontFamily: "Ubuntu, sans-serif",
+                  marginBottom: "10px",
+                }}
+              >
+                {error}
+              </div>
+            )}
+
+            <div className="container-login100-form-btn m-t-32">
+              <button
+                type="submit"
+                className="login100-form-btn"
+                style={{ fontFamily: "Ubuntu, sans-serif" }}
+              >
+                Login
+              </button>
+            </div>
+
+            <div className="text-center p-t-20">
+              <Link
+                to="/forgot-password"
+                className="txt1"
+                style={{
+                  fontFamily: "Ubuntu, sans-serif",
+                  display: "block",
+                  marginBottom: "10px",
+                  color: "#d41872",
+                }}
+              >
+                Forgot Password?
+              </Link>
+              <p>
+                Don’t have an account?
+                <Link
+                  to="/sign-up"
+                  className="txt1"
+                  style={{ fontFamily: "Ubuntu, sans-serif", color: "#d41872" }}
+                >
+                  Sign up
+                </Link>
+              </p>
+            </div>
+          </form>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default LoginScreen;
